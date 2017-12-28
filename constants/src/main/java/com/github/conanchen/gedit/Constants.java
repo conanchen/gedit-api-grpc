52d73f92dadaf69af122506dbaa0ab3b8a49bbbb
conanchen <conan8chan@yahoo.com>
2017-12-28 12:00:41 +0800

    remove build folder

---
+++ constants/src/main/java/com/github/conanchen/gedit/Constants.java
@@ -0,0 +1,5 @@
+package com.github.conanchen.gedit;
+
+public class Constants {
+    public static String VERSION = "1.0.0";
+}
--- constants/src/main/java/com/github/conanchen/yeamore/Constants.java
+++
@@ -1,5 +0,0 @@
-package com.github.conanchen.yeamore;
-
-public class Constants {
-    public static String VERSION = "1.0.0";
-}
